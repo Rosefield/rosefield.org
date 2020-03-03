37a2ae898bdb6d8b941826e9862606e1633b4541
Schuyler Rosefield <Rosefield@users.noreply.github.com>
2020-03-03 13:33:00 -0500

    Add research, lincoln.

--- site.py
+++ site.py
@@ -46,19 +46,47 @@ def common_replacements(name):
     return r 
 
 def name_to_id(name):
-    return name.lower().replace(' ','_')
+    return name.lower().replace(' ','_').replace(',','')
 
 
 def template(name, replacements):
-
     tmpl = load_template(name)
 
     return tmpl.format(**replacements)
 
-def create_link(article):
-
+def format_article_link(article):
     return f'<a class="project-link" id="{article.id}Link" href="#{article.id}">{article.name}</a>'
 
+def format_css(hash, file):
+    return f'<link rel="stylesheet" integrity="sha256-{hash}" href="{file}" />'
+
+def format_script(hash, file):
+    return f'<script async type="text/javascript" integrity="sha256-{hash}" src="{file}"></script>'
+
+def hash_file(name):
+    with open(name) as f:
+        content = f.read().encode('utf-8')
+        sha = sha256()
+        sha.update(content)
+
+        hash = sha.digest()
+        return b64encode(hash).decode('utf-8')
+
+
+def gen_script(name):
+
+    formatters = {"css": format_css, "js": format_script}
+
+    files[name] = get_file_time(name)
+    hash = hash_file(name)
+    type = name.split('.')[-1]
+    name = name + "?v=" + urllib.parse.quote_plus(hash)
+
+    return formatters[type](hash, name)
+
+'''
+Functions to generate the index page
+'''
 def index(scripts):
 
     name = 'Index'
@@ -66,13 +94,15 @@ def index(scripts):
     r['scripts'] = scripts
 
     articles = [about(), 
+                research(),
+                work(),
                 #interests(), 
                 projects(),  
                 resume()]
 
     r['articles'] = '\n'.join([a.content for a in articles])
     
-    r['links'] = '\n'.join([create_link(a) for a in articles])
+    r['links'] = '\n'.join([format_article_link(a) for a in articles])
     
     id = r['id']
     content = template('index', r)  
@@ -85,6 +115,21 @@ def about():
     content = template('about', r)
 
     return Article(r['id'], r['name'], content)
+
+def research():
+    name = 'Research'
+    r = common_replacements(name)
+    content = template('research', r)
+
+    return Article(r['id'], r['name'], content)
+
+def work():
+    name = 'Work'
+    r = common_replacements(name)
+    content = template('work', r)
+
+    return Article(r['id'], r['name'], content)
+    
 
 def projects():
     name = 'Projects'
@@ -107,36 +152,50 @@ def resume():
 
     return Article(r['id'], r['name'], content)
 
-def hash_file(name):
-    with open(name) as f:
-        content = f.read().encode('utf-8')
-        sha = sha256()
-        sha.update(content)
+'''
+Functions to generate candidacy page
+'''
+def candidacy(scripts):
+    name = 'Candidacy'
+    r = common_replacements(name)
+    r['scripts'] = scripts
+    r['title'] = 'Schuyler Rosefield Candidacy Form'
 
-        hash = sha.digest()
-        return b64encode(hash).decode('utf-8')
+    articles = [candidacy_statement(),
+                candidacy_link()] 
+                
+    r['articles'] = '\n'.join([a.content for a in articles])
+    
+    #r['links'] = '\n'.join([format_article_link(a) for a in articles])
+    
+    id = r['id']
+    content = template('candidacy', r)  
 
-def format_css(hash, file):
-    return f'<link rel="stylesheet" integrity="sha256-{hash}" href="{file}" />'
+    return Article(id, name, content)
 
-def format_script(hash, file):
-    return f'<script async type="text/javascript" integrity="sha256-{hash}" src="{file}"></script>'
+def candidacy_statement():
+    name = 'Statement by Advisor, abhi shelat'
+    r = common_replacements(name)
+    r['id'] = 'candidacy_statement'
+    content = template('candidacy_statement', r)
+
+    return Article(r['id'], r['name'], content)
+
+def candidacy_link():
+    name = 'Paper Link'
+    r = common_replacements(name)
+    content = template('candidacy_link', r)
+
+    return Article(r['id'], r['name'], content)
 
 
-def gen_script(name):
-
-    formatters = {"css": format_css, "js": format_script}
-
-    files[name] = get_file_time(name)
-    hash = hash_file(name)
-    type = name.split('.')[-1]
-    name = name + "?v=" + urllib.parse.quote_plus(hash)
-
-    return formatters[type](hash, name)
-
+'''
+Functions to run assemble the files
+'''
 def pre():
 
     other_files = ['files/resume.pdf',
+                    'files/DRSA.pdf',
                     'files/profile_image.jpg']
     
     for f in other_files:
@@ -164,6 +223,15 @@ def run():
         f.write(a.content)
         print(f'wrote {index_file}')
     files[index_file] = get_file_time(index_file)
+
+    
+    a = candidacy(scripts)
+    candidacy_file = a.id + '.html'
+    with open(candidacy_file, 'w') as f:
+        f.write(a.content)
+        print(f'wrote {candidacy_file}')
+    files[candidacy_file] = get_file_time(candidacy_file)
+
     post()
 
 def main():
